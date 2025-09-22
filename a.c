9c44e84f4de6503a672bdf626e974c4604430100
Richinb <134418579+Richinb@users.noreply.github.com>
2025-09-23 01:00:39 +0700

    Create a.c

---
+++ a.c
@@ -0,0 +1,39 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define SUCCESS 0
+#define ERROR -1
+
+void *mythread(void *arg)
+{
+	printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
+	return NULL;
+}
+
+int main()
+{
+	pthread_t tid;
+	int err;
+
+	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+	err = pthread_create(&tid, NULL, mythread, NULL);
+	if (err != SUCCESS)
+	{
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return ERROR;
+	}
+
+	err = pthread_join(tid, NULL); // tid, result of thread
+	if (err != SUCCESS)
+	{
+		printf("main: pthread_join() failed: %s\n", strerror(err));
+		return ERROR;
+	}
+	return 0;
+}
