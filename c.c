595814a6db577625224ab2727cc36b4169696dcd
Richinb <134418579+Richinb@users.noreply.github.com>
2025-10-15 20:11:56 +0700

    Create c.c

---
+++ c.c
@@ -0,0 +1,73 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define SUCCESS 0
+#define ERROR -1
+#define NOT_EQUAL 0
+
+int global_var = 100;
+
+void *mythread(void *arg)
+{
+    pid_t pid = getpid();
+    pid_t ppid = getppid();
+    pid_t tid_from_gettid = gettid();
+    pthread_t tid_from_create = *(pthread_t *)arg;
+    pthread_t tid_from_self = pthread_self();
+
+    int local_var = 2;
+    static int static_local_var = 3;
+    const int const_local_var = 4;
+
+    printf("mythread [%d %d %d]: Hello from mythread!\n", pid, ppid, tid_from_gettid);
+    printf("mythread ID (pthread_self): %lu\n", tid_from_self);
+    printf("Thread ID from pthread_create: %lu\n", tid_from_create);
+
+    if (pthread_equal(tid_from_create, tid_from_self) != NOT_EQUAL)
+    {
+        printf("pthread_self() and pthread_create() ID are EQUAL\n");
+    }
+    if (pthread_equal(tid_from_create, tid_from_self) == NOT_EQUAL)
+    {
+        printf("pthread_self() and pthread_create() ID are DIFFERENT\n");
+    }
+
+    if (pthread_equal(tid_from_create, tid_from_gettid) != NOT_EQUAL)
+    {
+        printf("gettid() and pthread_create() ID are EQUAL\n");
+    }
+    if (pthread_equal(tid_from_create, tid_from_gettid) == NOT_EQUAL)
+    {
+        printf("gettid() and pthread_create() ID are DIFFERENT\n");
+    }
+
+    printf("  Local variable: %p\n", (void *)&local_var);
+    printf("  Static local variable: %p\n", (void *)&static_local_var);
+    printf("  Const local variable: %p\n", (void *)&const_local_var);
+    printf("  Global variable: %p\n", (void *)&global_var);
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t tid;
+    int err;
+
+    printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+    err = pthread_create(&tid, NULL, mythread, NULL);
+    if (err != SUCCESS)
+    {
+        printf("main: pthread_create() failed: %s\n", strerror(err));
+        return ERROR;
+    }
+
+    return 0;
+}
