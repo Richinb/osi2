2170ac79cfd32829670606c8548b19b521d4263a
Richinb <134418579+Richinb@users.noreply.github.com>
2025-12-01 14:28:30 +0700

    Create b.c

---
+++ lab2/b.c
@@ -0,0 +1,44 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define SUCCESS 0
+#define ERROR -1
+
+void *mythread()
+{
+    printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
+    int *result = malloc(sizeof(int));
+    *result = 42;
+    printf("Value to return from mythread: %d\n", *result);
+    return (void *)result;
+}
+
+int main()
+{
+    pthread_t tid;
+    int err;
+
+    err = pthread_create(&tid, NULL, mythread, NULL);
+    if (err != SUCCESS)
+    {
+        printf("main: pthread_create() failed: %s\n", strerror(err));
+        return ERROR;
+    }
+
+    int *result;
+    err = pthread_join(tid, (void **)&result);
+    if (err != SUCCESS)
+    {
+        printf("main: pthread_create() failed: %s\n", strerror(err));
+        return ERROR;
+    }
+    printf("Value from mythread:%d \n", *result);
+    free(result);
+    return SUCCESS;
+}
