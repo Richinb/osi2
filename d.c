904a8417393fe828862536c5cbecc86a4a9aa601
Richinb <134418579+Richinb@users.noreply.github.com>
2025-12-01 14:26:23 +0700

    Update d.c

--- d.c
+++ d.c
@@ -6,22 +6,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define THREADS_COUNT 5
 #define SUCCESS 0
 #define ERROR -1
 #define NOT_EQUAL 0
+#define NUM_THREADS 5
 
 int global_var = 1;
 
 void *mythread(void *arg)
 {
+    int thread_num = *(int *)arg;
     pid_t pid = getpid();
     pid_t ppid = getppid();
     pid_t tid_from_gettid = gettid();
 
     int local_var = 2;
-    static int static_local_var = 3;
-    const int const_local_var = 4;
+    printf("\nThread %d\n", thread_num);
     printf("mythread [%d %d %d]: Hello from mythread!\n", pid, ppid, tid_from_gettid);
     printf("Before: local_var=%d, global_var=%d\n", local_var, global_var);
     local_var++;
@@ -35,19 +35,23 @@ void *mythread(void *arg)
 
 int main()
 {
-    pthread_t tid[THREADS_COUNT];
-    pthread_t *ptr_to_tids = tid;
+    pthread_t tids[NUM_THREADS];
+    int thread_nums[NUM_THREADS];
     int err;
 
     printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
 
-    err = pthread_create(&tid, NULL, mythread, (void *)&ptr_to_tids);
-    if (err != SUCCESS)
+    for (int i = 0; i < NUM_THREADS; i++)
     {
-        printf("main: pthread_create() failed: %s\n", strerror(err));
-        return ERROR;
+        thread_nums[i] = i;
+        err = pthread_create(&tids[i], NULL, mythread, &thread_nums[i]);
+        if (err != SUCCESS)
+        {
+            printf("main: pthread_create() failed for thread %d: %s\n", i, strerror(err));
+            return ERROR;
+        }
     }
 
     sleep(10);
-    return 0;
+    pthread_exit(NULL);
 }
